768c75c9a241984fd3d0ddba7cee7921bf77227b
Yan <elvhant@gmail.com>
2023-11-24 08:15:56 +0300

    Работа со свойствами и состоянием компонента

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,24 +1,61 @@
+import { Component } from 'react';
+
 import './employers-list-item.css'
 
-const EmployersListItem = () => {
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployersListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            promotion: false
+        }
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>        
-    )
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    onPromotion = () => {
+        this.setState(({promotion}) => ({
+            promotion: !promotion
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {increase, promotion} = this.state;
+
+        let className = "list-group-item d-flex justify-content-between";
+        if (increase) {
+            className += ' increase';
+        }
+        if (promotion) {
+            className += ' like';
+        }
+
+        return (
+            <li className={className}>
+                <span className="list-group-item-label" onClick={this.onPromotion}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>        
+        )
+    }
+
 }
 
 export default EmployersListItem;
